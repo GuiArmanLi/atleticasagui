6bb65c8ada120817d8f33ce48f51984808fe9838
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2024-12-21 22:06:51 -0300

    Adicionando endpoints de cada pagina no router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,26 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { PostsComponent } from './pages/posts/posts.component';
+import { BlogComponent } from './pages/blog/blog.component';
 import { DonateComponent } from './pages/donate/donate.component';
 import { GraffitiComponent } from './pages/graffiti/graffiti.component';
 import { StoreComponent } from './pages/store/store.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
-  },
-  { path: 'about', component: AboutComponent },
-  { path: 'donate', component: DonateComponent },
-  { path: 'graffiti', component: GraffitiComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'posts', component: PostsComponent },
-  { path: 'store', component: StoreComponent },
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    { path: 'about', component: AboutComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'donate', component: DonateComponent },
+    { path: 'graffiti', component: GraffitiComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'store', component: StoreComponent },
 
-  //Guardas e Resolvedores
-  //As guardas continuam funcionando da mesma forma, mas podem ser aplicadas também aos componentes independentes.
-  // path: 'admin',
-  // loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
-  // canActivate: [AuthGuard]
+    //Guardas e Resolvedores
+    //As guardas continuam funcionando da mesma forma, mas podem ser aplicadas também aos componentes independentes.
+    // path: 'admin',
+    // loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
+    // canActivate: [AuthGuard]
 ];
