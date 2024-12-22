111885c035f3b9f6f446511a58936758efdf9fd2
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2024-12-22 11:55:35 -0300

    Passando todo conteudo da home para components

---
+++ src/app/pages/home/offering/offerings.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  "selector": 'offerings-component',
+  "templateUrl": './offerings.component.html',
+  "styleUrl": './offerings.component.scss',
+  "standalone": true
+})
+export class OfferingsComponent { }
