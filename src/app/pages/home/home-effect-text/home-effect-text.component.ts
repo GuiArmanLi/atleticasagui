aa8d2729fa3c79d4f9ae95f8d1e2292280e9cd7c
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2024-12-22 11:55:35 -0300

    [Refactor] Distribui conteúdo de home-component para pequenos componentes

---
+++ src/app/pages/home/home-effect-text/home-effect-text.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  "selector": 'home-effect-text',
+  "templateUrl": './home-effect-text.component.html',
+  "styleUrl": './home-effect-text.component.scss',
+  "standalone": true
+})
+export class HomeEffectTextComponent { }
---
+++ src/app/pages/home/store-carousel/store-carousel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  "selector": 'store-carousel-component',
+  "templateUrl": './store-carousel.component.html',
+  "styleUrl": './store-carousel.component.scss',
+  "standalone": true
+})
+export class StoreCarouselComponent { }
