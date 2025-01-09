4f1355326f9237841893707725c32609c1129eef
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 17:58:30 -0300

    [Refactor] Padroniza header-component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { MenuComponent } from "./menu/menu.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
-    "selector": 'header-component',
-    "templateUrl": './header.component.html',
-    "styleUrl": './header.component.scss',
-    "standalone": true,
-    "imports": [MenuComponent]
+  selector: 'header-component',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  standalone: true,
+  imports: [MenuComponent, RouterModule]
 })
 export class HeaderComponent { }
