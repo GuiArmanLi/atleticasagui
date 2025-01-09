323753ed6dae7ccd18b54c663bbfffe6915d6a4a
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:12:31 -0300

    Adicionando componente History and Curiosities da pagina About

---
+++ src/app/pages/about/history-and-curiosities/history-and-curiosities.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "history-and-curiosities-component",
+  templateUrl: "./history-and-curiosities.component.html",
+  styleUrl: "./history-and-curiosities.component.scss",
+  standalone: true
+})
+export class HistoryAndCuriositiesComponent { }
