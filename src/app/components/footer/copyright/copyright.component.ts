cdf2e5fe470ef61aabba86e5d43968c0df1d8505
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 17:55:35 -0300

    [Refactor] Padroniza copyright-component

--- src/app/components/footer/copyright/copyright.component.ts
+++ src/app/components/footer/copyright/copyright.component.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'copyright-component',
-    template: `
+  selector: 'copyright-component',
+  template: `
+    <!--Adicionar link com politica de privacidade e opcao de mudar a linguagem do site-->
         <aside class="copyright">
             <p>Copyright 2024 © Todos os direitos reservados</p>
         </aside>
     `,
-    styleUrl: './copyright.component.scss',
-    standalone: true
+  styleUrl: './copyright.component.scss',
+  standalone: true
 })
 export class CopyrightComponent { }
