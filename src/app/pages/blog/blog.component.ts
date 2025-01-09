64d50ba0e536861f1687db35cbb813c56830aedb
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:10:31 -0300

    Adicionando componente da pagina Blog

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 
 @Component({
-  "selector": 'blog-component',
-  "templateUrl": './blog.component.html',
-  "styleUrl": './blog.component.scss',
-  "standalone": true,
-  "imports": []
+  selector: 'blog-component',
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.scss',
+  standalone: true,
+  imports: []
 })
 export class BlogComponent { }
