e008e24cf105dc3bcc7bfbeb6a52f3931559c27d
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:08:49 -0300

    Adicionando componente da pagina Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component } from "@angular/core";
 import { SliderComponent } from "./slider/slider.component";
-import { StoreCarouselComponent } from "./store-carousel/store-carousel.component";
-import { BlogCarouselComponent } from "./blog-carousel/blog-carousel.component";
 import { HomeParallaxComponent } from "./home-parallax/home-parallax.component";
 import { OfferingsComponent } from "./offering/offerings.component";
+import { HomeEffectTextComponent } from "./home-effect-text/home-effect-text.component";
+import { BackgorundMapAndContactsSocialMedias } from "./backgorund-map-and-contacts-social-medias/backgorund-map-and-contacts-social-medias.component";
+import { StoreContentComponent } from "./store-content/store-content.component";
+import { BlogContentComponent } from "./blog-content/blog-content.component";
 
 @Component({
-  "selector": 'home-component',
-  "templateUrl": './home.component.html',
-  "styleUrl": './home.component.scss',
-  "standalone": true,
-  "imports": [SliderComponent, StoreCarouselComponent, BlogCarouselComponent, HomeParallaxComponent, OfferingsComponent],
+  selector: 'home-component',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  standalone: true,
+  imports: [
+    SliderComponent,
+    HomeEffectTextComponent,
+    StoreContentComponent,
+    BlogContentComponent,
+    HomeParallaxComponent,
+    OfferingsComponent,
+    BackgorundMapAndContactsSocialMedias
+  ],
 })
 export class HomeComponent { }
