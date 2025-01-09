d0eab04069284dcf443360d740891f383ce7ab8d
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:13:36 -0300

    [Feat] Adiciona history-and-curiosities-component

---
+++ src/app/components/map-and-contacts/map/map.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'map-component',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss',
+  standalone: true
+})
+export class MapComponent { }
