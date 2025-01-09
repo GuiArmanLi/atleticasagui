fb7d7734e3ed6ac4dcbaed43ee7959e733623505
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:05:58 -0300

    Adicionando componente Effect Text da pagina Home

--- src/app/pages/home/home-effect-text/home-effect-text.component.ts
+++ src/app/pages/home/home-effect-text/home-effect-text.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 @Component({
-  "selector": 'home-effect-text',
-  "templateUrl": './home-effect-text.component.html',
-  "styleUrl": './home-effect-text.component.scss',
-  "standalone": true
+  selector: 'home-effect-text',
+  templateUrl: './home-effect-text.component.html',
+  styleUrl: './home-effect-text.component.scss',
+  standalone: true,
+  imports: [RouterModule]
 })
 export class HomeEffectTextComponent { }
