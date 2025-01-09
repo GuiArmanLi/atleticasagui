ac2fc61b727e0c21229408ffd4e886bf47191fa2
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:14:22 -0300

    Adicionando componente Social Medias

---
+++ src/app/components/social-medias/social-media.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'social-media-component',
+  templateUrl: './social-media.component.html',
+  styleUrl: './social-media.component.scss',
+  standalone: true
+})
+export class SocialMediaComponent { }
