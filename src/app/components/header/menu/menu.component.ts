3b6793371441de4dfcc72db0c23f6f257408993b
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 17:57:56 -0300

    Adicionando components Menu

--- src/app/components/header/menu/menu.component.ts
+++ src/app/components/header/menu/menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-    "selector": 'menu-component',
-    "templateUrl": './menu.component.html',
-    "styleUrl": './menu.component.scss',
-    "standalone": true
+  selector: 'menu-component',
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss',
+  standalone: true,
+  imports: [RouterModule]
 })
 export class MenuComponent { }
