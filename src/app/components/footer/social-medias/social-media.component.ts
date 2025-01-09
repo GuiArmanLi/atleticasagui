c41a5a320fe367d858fe3b24865d431b377656fe
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 17:54:43 -0300

    Deletando arquivos excluidos

--- src/app/components/footer/social-medias/social-media.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'social-media-component',
-    template: './social-media.component.html',
-    styleUrl: './social-media.component.scss',
-    standalone: true
-})
-export class SocialMediaComponent {}
--- src/app/pages/home/store-carousel/store-carousel.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  "selector": 'store-carousel-component',
-  "templateUrl": './store-carousel.component.html',
-  "styleUrl": './store-carousel.component.scss',
-  "standalone": true
-})
-export class StoreCarouselComponent { }
