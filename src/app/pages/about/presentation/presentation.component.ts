5a62750af517bf1c1777b0cb4d61eefdf86cf56c
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:16:25 -0300

    Adicionando componente Presentation da pagina About

---
+++ src/app/pages/about/presentation/presentation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "presentation-component",
+  templateUrl: "./presentation.component.html",
+  styleUrl: "./presentation.component.scss",
+  standalone: true
+})
+export class PresentationComponent { }
