ac79d56b4bd1aa8ee3f44ddc2f245c85028fe7a7
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 17:54:43 -0300

    Deletando arquivos excluidos

--- src/app/pages/home/blog-carousel/blog-carousel.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  "selector": 'blog-carousel-component',
-  "templateUrl": './blog-carousel.component.html',
-  "styleUrl": './blog-carousel.component.scss',
-  "standalone": true
-})
-export class BlogCarouselComponent { }
