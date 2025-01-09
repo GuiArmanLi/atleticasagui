cda8f1f72d19221764f1eab28b3c45568199874c
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:04:31 -0300

    Adicionando componentes Blog e Store da pagina Home

---
+++ src/app/pages/home/blog-content/blog-content.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { SliderLogic } from "../../../components/slider-logic/slider-logic";
+
+@Component({
+  selector: "blog-content-component",
+  templateUrl: "./blog-content.component.html",
+  styleUrls: ["./blog-content.component.scss"], // Fixed
+  standalone: true,
+  imports: [RouterModule],
+})
+export class BlogContentComponent extends SliderLogic {
+  override sliderName = "slider-blog"; // Match the DOM ID of the slider
+  override images = [
+    "https://static.todamateria.com.br/upload/ed/it/editorialjornal-cke.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyX22_ZdKs0JYvqD71xM-uMleXhQKNBGa83qhdtwFRD84r-3D_mm47R_3tL0tRC3JiQvg&usqp=CAU",
+  ];
+}
---
+++ src/app/pages/home/store-content/store-content.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { SliderLogic } from "../../../components/slider-logic/slider-logic";
+
+@Component({
+  selector: 'store-content-component',
+  templateUrl: './store-content.component.html',
+  styleUrl: './store-content.component.scss',
+  standalone: true,
+  imports: [RouterModule]
+})
+export class StoreContentComponent extends SliderLogic {
+  override sliderName = "slider-store";
+
+  //Fazer requisicao a API
+  override images = [
+    "https://cdn.iset.io/assets/34628/produtos/13/par-caneca-alca-coracao.jpg",
+    "https://down-br.img.susercontent.com/file/036c59d68325e749f546690733c107fe"
+  ];
+}
