4c91f66af929ae2430ef1fb8b7087c39c64b7e09
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:17:19 -0300

    Adicionando componente da pagina About

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,15 @@
 import { Component } from "@angular/core";
 import { CompanyIdentitiesComponent } from "./company-identities/company-identities.component";
+import { MembersComponent } from "./members/members.component";
+import { PresentationComponent } from "./presentation/presentation.component";
+import { HistoryAndCuriositiesComponent } from "./history-and-curiosities/history-and-curiosities.component";
+import { HowToParticipateComponent } from "./how-to-participate/how-to-participate.component";
 
 @Component({
-  "selector": 'about-component',
-  "templateUrl": './about.component.html',
-  "styleUrl": './about.component.scss',
-  "standalone": true,
-  "imports": [CompanyIdentitiesComponent]
+  selector: 'about-component',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+  standalone: true,
+  imports: [CompanyIdentitiesComponent, MembersComponent, PresentationComponent, HistoryAndCuriositiesComponent, HowToParticipateComponent]
 })
 export class AboutComponent { }
