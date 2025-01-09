8970d3cb4045320cd2860555191645ebf8e94ceb
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:11:50 -0300

    Adicionando componente Company Identities da pagina About

--- src/app/pages/about/company-identities/company-identities.component.ts
+++ src/app/pages/about/company-identities/company-identities.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
-    "selector": 'company-identities',
-    "templateUrl": './company-identities.component.html',
-    "styleUrl": './company-identities.component.scss',
-    "standalone": true
+  selector: 'company-identities',
+  templateUrl: './company-identities.component.html',
+  styleUrl: './company-identities.component.scss',
+  standalone: true
 })
 export class CompanyIdentitiesComponent { }
