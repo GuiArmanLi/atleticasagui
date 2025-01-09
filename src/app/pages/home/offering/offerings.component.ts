3de391055f8ee6b66db1138ccd5aacdd0456a185
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-09 18:07:04 -0300

    Adicionando componente Offering da pagina Home

--- src/app/pages/home/offering/offerings.component.ts
+++ src/app/pages/home/offering/offerings.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  "selector": 'offerings-component',
-  "templateUrl": './offerings.component.html',
-  "styleUrl": './offerings.component.scss',
-  "standalone": true
+  selector: 'offerings-component',
+  templateUrl: './offerings.component.html',
+  styleUrl: './offerings.component.scss',
+  standalone: true,
+  imports: [RouterModule]
 })
 export class OfferingsComponent { }
