0428457ebdb647361377008582d54bc73ffab303
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-11 00:35:31 -0300

    Corrigindo nomenclatura do compoenente info-map-with-contacts e movendo a pasta components para shared

--- src/app/components/footer/donate/donate.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'donate-component',
-    templateUrl: './donate.component.html',
-    styleUrl: './donate.component.scss',
-    standalone: true
-})
-export class DonateComponent { }
--- src/app/components/map-and-contacts/map/map.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'map-component',
-  templateUrl: './map.component.html',
-  styleUrl: './map.component.scss',
-  standalone: true
-})
-export class MapComponent { }
