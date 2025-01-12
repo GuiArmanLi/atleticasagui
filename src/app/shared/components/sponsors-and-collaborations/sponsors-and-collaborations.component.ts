fd0d3f81b632cdd9249cee5ea1301c063c28d4d4
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-12 04:51:55 -0300

    Adicionando componente sponsors-and-collaborations e organizando pasta de icones

---
+++ src/app/shared/components/sponsors-and-collaborations/sponsors-and-collaborations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "sponsors-and-collaborations-component",
+  templateUrl: "./sponsors-and-collaborations.component.html",
+  styleUrls: ["./sponsors-and-collaborations.component.scss"],
+  standalone: true
+})
+export class SponsorsAndCollaborations {
+  
+}
