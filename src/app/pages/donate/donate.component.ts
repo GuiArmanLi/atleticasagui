e7bac5f8eb82400714a290dcbbb9f0548425d1fe
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-12 04:54:07 -0300

    Finalizando template e estilo da página Donate

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
+import { SponsorsAndCollaborations } from "../../shared/components/sponsors-and-collaborations/sponsors-and-collaborations.component";
 
 @Component({
   selector: 'donate-component',
   templateUrl: './donate.component.html',
   styleUrl: './donate.component.scss',
   standalone: true,
-  imports: []
+  imports: [SponsorsAndCollaborations]
 })
 export class DonateComponent { }
