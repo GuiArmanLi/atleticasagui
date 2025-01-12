2977df9ab3ec405149d64ed62dfd426f039d2000
GuiArmanLi <guilhermesantanapessa12@gmail.com>
2025-01-12 04:53:03 -0300

    Adicionando redes sociais dinamicas com diretiva Input

--- src/app/shared/components/social-medias/social-media.component.ts
+++ src/app/shared/components/social-medias/social-media.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { SocialMedia } from "../../models/member";
 
 @Component({
   selector: 'social-media-component',
@@ -6,4 +7,17 @@ import { Component } from "@angular/core";
   styleUrl: './social-media.component.scss',
   standalone: true
 })
-export class SocialMediaComponent { }
+export class SocialMediaComponent implements OnInit {
+  @Input("social-media")
+  socialMedia!: SocialMedia;
+
+  ngOnInit(): void {
+    if (this.socialMedia == null) {
+      this.socialMedia = {
+        whatsapp: "https://wa.me/5511993006741",
+        instagram: "https://www.instagram.com/atleticasagui_fatecferraz/",
+        linkedin: "https://www.linkedin.com/"
+      };
+    }
+  }
+}
